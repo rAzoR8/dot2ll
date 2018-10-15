354d209385956bd3113f03cb455ed0ce48d49da4
Fabian Wahlster <razor@singul4rity.com>
2018-10-15 12:54:41 +0200

    removed

--- include/OpenTree.h
+++ include/OpenTree.h
@@ -23,7 +23,6 @@ struct OpenTreeNode
     BasicBlock* pBB = nullptr;
     bool bVisited = false; // has been added to the OT
     bool bFlow = false; // is a flowblock
-    bool bRemoved = false; // for debugging
 
     struct Flow
     {
