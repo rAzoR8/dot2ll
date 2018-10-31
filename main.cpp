556bcd727017a51d97f1993b67ae40e5c1df5110
Fabian Wahlster <razor@singul4rity.com>
2018-10-31 13:24:34 +0100

    reconverge input dir

--- main.cpp
+++ main.cpp
@@ -5,12 +5,94 @@
 #include "OpenTree.h"
 #include "InstructionSetLLVMAMD.h"
 #include "CheckReconvergence.h"
+#include <filesystem>
+
+static const std::wstring OrderNames[] =
+{
+    L"DepthFirst",
+    L"BreadthFirst",
+    L"DepthFirstDom",
+};
+
+void dot2ll(const std::string& _sDotFile, const NodeOrdering::Type _kOrder, const bool _bReconv = false)
+{
+    DotGraph dotin = DotParser::ParseFromFile(_sDotFile);
+
+    if (dotin.GetNodes().empty() == false)
+    {
+        Function func = Dot2CFG::Convert(dotin);
+
+        const bool bInputReconverging = CheckReconvergence::IsReconverging(func, !_bReconv, true);
+
+        HLOG("Reconverging %s '%s' [Order: %s Reconv: %s]", WCSTR(_sDotFile), WCSTR(dotin.GetName()), WCSTR(OrderNames[_kOrder]), bInputReconverging ? L"true" : L"false");
+
+        std::string sOutName = dotin.GetName();
+
+        if (_bReconv)
+        {
+            sOutName += "_reconv";
+
+            func.EnforceUniqueExitPoint();
+
+            NodeOrder InputOrdering;
+
+            switch (_kOrder)
+            {
+            case NodeOrdering::DepthFirst:
+                InputOrdering = NodeOrdering::ComputeDepthFirst(func.GetEntryBlock());
+                sOutName += "_df";
+                break;
+            case NodeOrdering::BreadthFirst:
+                InputOrdering = NodeOrdering::ComputeBreadthFirst(func.GetEntryBlock());
+                sOutName += "_bf";
+                break;
+            case NodeOrdering::DepthFirstDom:
+            default:
+                InputOrdering = NodeOrdering::ComputePaper(func.GetEntryBlock(), func.GetExitBlock());
+                sOutName += "_dfd";
+                break;
+            }
+
+            // reconverge using InputOrdering
+            OpenTree OT;
+            OT.Process(InputOrdering);
+
+            func.Finalize();
+
+            const bool bOutputReconverging = CheckReconvergence::IsReconverging(func, true);
+            hlx::Logger::Instance()->Log(bOutputReconverging ? hlx::kMessageType_Info : hlx::kMessageType_Error, WFUNC, WFILE, __LINE__, L"Function %s reconverging!\n", bOutputReconverging ? L"is" : L"is NOT");
+
+            HASSERTD(bOutputReconverging, "Function %s is NOT reconverging!", WCSTR(func.GetName()));
+
+            std::ofstream dotout(sOutName + ".dot");
+
+            if (dotout.is_open())
+            {
+                DotWriter::WriteToStream(CFG2Dot::Convert(func.GetCFG(), func.GetName()), dotout);
+
+                dotout.close();
+            }
+        }
+
+        std::ofstream ll(sOutName + ".ll");
+
+        if (ll.is_open())
+        {
+            func.Finalize();
+
+            InstructionSetLLVMAMD isa;
+            isa.SerializeListing(func, ll);
+
+            ll.close();
+        }
+    }
+}
 
 int main(int argc, char* argv[])
 {
     hlx::Logger::Instance()->WriteToStream(&std::wcout);
 
-    std::string sInputFile;
+    std::filesystem::path InputPath;
 
     if (argc < 2)
         return 1;
@@ -46,75 +128,25 @@ int main(int argc, char* argv[])
         //}
         else
         {
-            sInputFile = token;
+            InputPath = token;
         }
     }
 
-    DotGraph dotin = DotParser::ParseFromFile(sInputFile);
-
-    if (dotin.GetNodes().empty() == false)
+    if (std::filesystem::is_directory(InputPath))
     {
-        Function func = Dot2CFG::Convert(dotin);
-
-        const bool bReconverges = CheckReconvergence::IsReconverging(func, !bReconv, true);
-        HLOG("Input Function %s is reconverging: %s", WCSTR(func.GetName()), bReconverges ? L"true" : L"false");
-
-        std::string sOutLL = dotin.GetName();
-
-        if (bReconv)
+        for (const auto& Entry : std::filesystem::directory_iterator(InputPath))
         {
-            sOutLL += "_reconv";
-
-            func.EnforceUniqueExitPoint();
-
-            NodeOrder InputOrdering;
-
-            switch (kOrder)
+            if (Entry.is_directory() == false && Entry.path().extension() == ".dot")
             {
-            case NodeOrdering::DepthFirst:
-                InputOrdering = NodeOrdering::ComputeDepthFirst(func.GetEntryBlock());
-                break;
-            case NodeOrdering::BreadthFirst:
-                InputOrdering = NodeOrdering::ComputeBreadthFirst(func.GetEntryBlock());
-                break;
-            case NodeOrdering::DepthFirstDom:
-            default:
-                InputOrdering = NodeOrdering::ComputePaper(func.GetEntryBlock(), func.GetExitBlock());
-                break;
+                dot2ll(Entry.path().string(), kOrder, bReconv);
             }
-
-            // reconverge using InputOrdering
-            OpenTree OT;
-            OT.Process(InputOrdering);
-
-            func.Finalize();
-
-            HASSERT(CheckReconvergence::IsReconverging(func, true), "Function %s is NOT reconverging!", WCSTR(func.GetName()));
-
-            const std::string sOutDot = dotin.GetName() + "_reconv.dot";
-            std::ofstream dotout(sOutDot);
-
-            if (dotout.is_open())
-            {
-                DotWriter::WriteToStream(CFG2Dot::Convert(func.GetCFG(), func.GetName()), dotout);
-
-                dotout.close();
-            }
-        }
-
-        sOutLL += ".ll";
-        std::ofstream ll(sOutLL.c_str());
-
-        if (ll.is_open())
-        {
-            func.Finalize();
-            
-            InstructionSetLLVMAMD isa;
-            isa.SerializeListing(func, ll);
-
-            ll.close();
         }
     }
+    else
+    {
+        dot2ll(InputPath.string(), kOrder, bReconv);
+    }
+    
 
     return 0;
 }
